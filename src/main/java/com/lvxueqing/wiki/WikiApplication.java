1089787edb3303fcd46ba02b39d0642230c325d6
lvxueqinga <2842726905@qq.com>
2022-06-18 14:24:29 +0800

    修改启动团

--- src/main/java/com/lvxueqing/wiki/WikiApplication.java
+++ src/main/java/com/lvxueqing/wiki/WikiApplication.java
@@ -3,11 +3,17 @@ package com.lvxueqing.wiki;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class WikiApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(WikiApplication.class, args);
+		System.out.println();
+
+
+
 	}
 
 }
