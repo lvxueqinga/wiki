0433df205f56c38dd935751fc23fff10fb7bf08a
lvxueqinga <2842726905@qq.com>
2022-06-18 16:18:15 +0800

    增加 自定义配置项示例

--- src/main/java/com/lvxueqing/wiki/controller/TestController.java
+++ src/main/java/com/lvxueqing/wiki/controller/TestController.java
@@ -1,14 +1,27 @@
 package com.lvxueqing.wiki.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 public class TestController {
 
-    @RequestMapping("/hello")
+    @Value("${test.Hello:MoRenZhi}")
+    private String testHello;
+
+//    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String hello(){
-        return "Hello world";
+        return "Hello world" + testHello;
     }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+        return "Hello world, i know your name: " + name;
+    }
+
+
 }
