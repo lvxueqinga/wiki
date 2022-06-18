cc30c33e9739ede801e8f639c71db6e87298a9f8
lvxueqinga <2842726905@qq.com>
2022-06-18 15:39:57 +0800

    hello接口 + ComponentScan注解

--- src/main/java/com/lvxueqing/wiki/config/WikiApplication.java
+++ src/main/java/com/lvxueqing/wiki/config/WikiApplication.java
@@ -1,11 +1,17 @@
-package com.lvxueqing.wiki;
+package com.lvxueqing.wiki.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
+@ComponentScan("com.lvxueqing")
 public class WikiApplication {
+    /**
+     * ComponentScan  不加这个注释的话 默认只会扫描当前包及其子包，其他包的就会404
+     * @param args
+     */
 
 
 	public static void main(String[] args) {
