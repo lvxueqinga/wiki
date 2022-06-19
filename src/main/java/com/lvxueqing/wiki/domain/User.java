e6cc7a0eec22e226769af2726b8d59be6a7622d0
lvxueqinga <2842726905@qq.com>
2022-06-19 17:55:44 +0800

    集成 mybatis-generate 自动生成 user类代码

---
+++ src/main/java/com/lvxueqing/wiki/domain/User.java
@@ -0,0 +1,46 @@
+package com.lvxueqing.wiki.domain;
+
+public class User {
+    private Long id;
+
+    private String username;
+
+    private String passwd;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", username=").append(username);
+        sb.append(", passwd=").append(passwd);
+        sb.append("]");
+        return sb.toString();
+    }
+}
