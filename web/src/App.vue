26a8df419b181c4c86d39a2155511c16051ff9b1
lvxueqinga <2842726905@qq.com>
2022-06-21 01:53:11 +0800

    制作footer组件

--- web/src/App.vue
+++ web/src/App.vue
@@ -2,20 +2,20 @@
     <a-layout id="components-layout-demo-top-side-2">
         <the-header></the-header>
         <router-view/>
+        <the-footer></the-footer>
 
-        <a-layout-footer style="text-align: center">
-            lxq电子书11 ©2022 Created by Ant UED
-        </a-layout-footer>
     </a-layout>
 </template>
 <script>
     import {defineComponent} from "vue";
 
     import TheHeader from '@/components/the-header.vue'
+    import TheFooter from '@/components/the-footer.vue'
     export default defineComponent({
         name: 'app',
         components: {
             TheHeader,
+            TheFooter,
 
         },
     })
